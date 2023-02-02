532825e5b5aa6405330e49d97fed00904c991e07
ilijasam <124308255+ilijasam@users.noreply.github.com>
2023-02-02 22:11:28 +0100

    Added some comments.

--- S001_dat2mat.m
+++ S001_dat2mat.m
@@ -1,23 +1,24 @@
-%       this script is reading the raw BGULS data and writing it to a .mat file
+%       this script is reading the raw Beaufort Gyre Upward Looking Sonar data and writing into a .mat file
+%       On date 02.02.2023, the data was available here: https://www2.whoi.edu/site/beaufortgyre/data/mooring-data/
+
 
 clear all
 
 addpath('Supporting Files\') %folder containing supporting scripts
 
-cf = pwd;   % reading current folder
+cf = pwd;   % reading current folder in order to be able to go back to it once the script is done (last line)
 df = 'D:\Moorings'; % specifing the folder containing the mooring data ()
 
-for year = 2017
+for year = 2013
    cd(fullfile(df, [num2str(year),'-',num2str(year+1)])) % opening folder with year's data (e.g., "D:\Moorings\2017-2018")
    listing = dir; % reading data files available
    
    no_files = length(listing)-2;
    
-%    for file = 1:no_files
-   for file = 1:1
+%    for file = 1:no_files  % use this lin if all of the files (locations) for the given year are to be extracted 
+   for file = 1:1           % use this line if only one file (location) is to be extracted
         fid = fopen(listing(file+2).name);
         c = textscan(fid,'%d %d %f','HeaderLines', 2 );
-
 
         %     read the date, time and data from file
         date = c{1};
@@ -46,8 +47,9 @@ for year = 2017
         frewind(fid);
         Data.tline = fgets(fid);
         Data.file_name = listing(file+2).name;
-        %     save the .mat file of the extracted data
-        save(['D:\Moorings\MAT files\' Data.file_name(1:end-4) '.mat'],'Data' )
+        %     save the .mat file of the extracted data to the defined
+        %     folder
+        save(['D:\Moorings\test\' Data.file_name(1:end-4) '.mat'],'Data' )
         %     display the year and the file step
         disp(['File  :  ' num2str(file) ' / ' num2str(no_files) '    |    ' 'Year  :  ' num2str(year)])
    end
